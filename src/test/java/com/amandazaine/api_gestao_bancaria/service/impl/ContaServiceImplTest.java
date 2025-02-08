0296e0f13148cfdc91c0ef18afa49a2210ee004f
Amanda Zaine <amandazaine@outlook.com>
2025-02-08 02:12:41 -0300

    Criação de teste unitários com Junit e Mockito

---
+++ src/test/java/com/amandazaine/api_gestao_bancaria/service/impl/ContaServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.amandazaine.api_gestao_bancaria.service.impl;
+
+import com.amandazaine.api_gestao_bancaria.dto.ContaDTO;
+import com.amandazaine.api_gestao_bancaria.entity.ContaEntity;
+import com.amandazaine.api_gestao_bancaria.repository.ContaRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
+//@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ContaServiceImplTest {
+
+    @Mock
+    private ContaRepository contaRepository;
+
+    @InjectMocks
+    private ContaServiceImpl contaService;
+
+    @Nested
+    class saveConta {
+
+        private ContaDTO contaDTO;
+        ContaEntity contaEntityTest;
+
+        @BeforeEach
+        void setUp() {
+            contaDTO  = new ContaDTO(1111, 100f);
+            contaEntityTest = new ContaEntity(contaDTO);
+        }
+
+        @Test
+        @DisplayName("Deve criar um conta com sucesso")
+        public void deveCriarUmaConta() {
+            Optional<ContaEntity> contaEntityOptional = Optional.empty();
+
+            Mockito.doReturn(contaEntityOptional).when(contaRepository).findById(contaDTO.getNumeroConta());
+
+            contaService.saveConta(contaDTO);
+
+            Mockito.verify(contaRepository, times(1)).save(contaEntityTest);
+        }
+
+        @Test
+        @DisplayName("Não deve criar uma conta e deve ser lançada uma exceção")
+        public void naoDeveCriarUmaConta() {
+            Optional<ContaEntity> contaEntityOptional = Optional.of(contaEntityTest);
+
+            Mockito.doReturn(contaEntityOptional).when(contaRepository).findById(contaDTO.getNumeroConta());
+
+            //Mockito.when(contaRepository.findById(contaDTO.getNumeroConta())).thenThrow(IllegalArgumentException.class);
+
+            Assertions.assertThrows(
+                    IllegalArgumentException.class,
+                    () -> contaService.saveConta(contaDTO)
+            );
+
+            Mockito.verify(contaRepository, never()).save(any(ContaEntity.class));
+        }
+    }
+
+}
